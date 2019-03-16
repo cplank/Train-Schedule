893b44f3fe0c061be9c5071571e1bd3d3624cc6f
cplank <cplankshepard@gmail.com>
2019-03-15 20:47:27 -0700

    done!

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Train-Schedule
+A simple program that returns a train schedule of your creation.
+
+## Description ##
+Made using Firebase to send and receive information in real time
+
+This program also uses:
+
+* jQuery 
+* Bootstrap 4.3
+* JavaScript 
+* Moment.js
+
+## Basics ##
+
+The onclick handles taking the user's input and saving it to variables that are then pushed Firebase in an object. Each time Firebase receives new information, it stores that in the snapshot which is then saved to new variables so next arrival and minutes away can be calculated, as well as appending to the HTML.
+
+## Roadmap ##
+
+I'd say this accomplished all it was meant to!
+
+## Shout Outs ##
+Butch Cassidy and the Sundance Kid are my favorite bromance. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+//Setting up Firebase
+
 var config = {
     apiKey: "AIzaSyDTqSbPIrYx4N-E-aeF6wXELbwKNX20pW8",
     authDomain: "doomed-trains-schedule.firebaseapp.com",
@@ -6,23 +8,24 @@ var config = {
     storageBucket: "doomed-trains-schedule.appspot.com",
     messagingSenderId: "292028498123"
 };
+
 firebase.initializeApp(config);
 
+//storing Firebase in an easyily accessible 
 let database = firebase.database();
 
-// 2. Button for adding Employees
+//Making the onclick work for the add train button
 $("#add-train-btn").on("click", function (event) {
+    //Don't forget to clear default because submit buttons are dumb
     event.preventDefault();
 
-    // Grabs user input
+    // Let's make some variables!
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var firstTrain = $("#first-train-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
 
-
-
-    // Creates local "temporary" object for holding employee data
+    // Storing all those variables in an object for easy access later
     var newTrain = {
         name: trainName,
         role: destination,
@@ -30,43 +33,33 @@ $("#add-train-btn").on("click", function (event) {
         rate: frequency
     };
 
-    // Uploads employee data to the database
+    // Pushes that object to Firebase so it knows wtf is going on
     database.ref().push(newTrain);
 
-    // Logs everything to console
-    console.log(newTrain.trainName);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrain);
-    console.log(newTrain.frequency);
-
-
-    // Clears all of the text-boxes
+    // Reset the input boxes
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// Grabbing our object from Firebase when something new is added so we can reference it later for the HTML and 
+//reference it now for figuring out time
 database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
-
 
     var trainName = childSnapshot.val().name;
     var destination = childSnapshot.val().role;
     var firstTrain = childSnapshot.val().start;
     var frequency = childSnapshot.val().rate;
 
-
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrain);
-    console.log(frequency);
-
+    //Passing the arguments of frequeny and firstTrain (created above) these variables will store the next train's
+    //arrival and and how many minutes away it is. We have to call this function inside the database.ref().on function
+    //because that's where we declare these variables. They only exist here. 
     let minAway = getMinutesAway(frequency, firstTrain);
     let nextTrain = getNextArrival(minAway);
 
 
+    //Add the information a new HTML row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
@@ -75,23 +68,40 @@ database.ref().on("child_added", function (childSnapshot) {
         $("<td>").text(minAway)
     );
 
-
+    //append our new row to the table body
     $("#current-train-table > tbody").append(newRow);
 });
 
+
+//TIME THINGS
+
+//This function takes parameters (I called them frequency and firstTrain for easy of understanding) and uses moment.js
+//to take firstTrain's date, make sure it's in military time, and then subtracts a year so our predictions are never negative
 function getMinutesAway(frequency, firstTrain) {
     var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
 
+
+    //This variable stores the difference between today's time and firstTimeConverted in minutes
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
+    //This stores the remainder of diffTime and frequency
     var remainder = diffTime % frequency;
 
+    //And subtracts that remainder from frequency for a result of minutesTillTrain
     var minutesTillTrain = frequency - remainder;
+
+    //Returns minutesTillTrain so we can reference it when we call this function
     return minutesTillTrain;
 }
 
+//This function takes the parameter of minAway, which was created above
 function getNextArrival(minAway) {
-    var nextTrain = moment().add(minAway, "minutes");
+
+    //nextTrain stores the current time and adds minAway and formats that to minutes
+
+    let nextTrain = moment().add(minAway, 'minutes').format('HH:mm')
+
+    //The result is that we return how many minutes until the next train
     return nextTrain;
 }
 
